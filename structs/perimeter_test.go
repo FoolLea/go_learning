e89f74487dc9479c3aaeb9a2e585fbea144f82b6
“jialin” <914001513@qq.com>
2020-08-04 23:48:17 +0800

    接口

--- structs/perimeter_test.go
+++ structs/perimeter_test.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-08-03 21:07:52
- * @LastEditTime: 2020-08-03 23:47:06
+ * @LastEditTime: 2020-08-04 23:25:46
  * @LastEditors: your name
  * @Description:
  * @FilePath: /learn-go-with-tests/structs/perimeter_test.go
@@ -25,7 +25,8 @@ func TestArea(t *testing.T) {
 		// got := Area(10.0, 10.0)
 		// want := 100.0
 		rectangle := Rectangle{10.0, 10.0}
-		got := rectangle.Area(rectangle)
+		// got := rectangle.Area(rectangle)
+		got := rectangle.Area()
 		want := 100.0
 
 		if got != want {
@@ -35,11 +36,33 @@ func TestArea(t *testing.T) {
 
 	t.Run("circle", func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area(circle)
-		want := 314.16
+		// got := circle.Area(circle)
+		got := circle.Area()
+		want := 314.1592653589793
 
 		if got != want {
 			t.Errorf("got %.2f want %.2f", got, want)
 		}
 	})
+}
+
+func TestArea2(t *testing.T) {
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	}
+
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{11, 11}
+		checkArea(t, rectangle, 121)
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
+
 }
--- structs/premeter.go
+++ structs/premeter.go
@@ -1,13 +1,17 @@
 /*
  * @Author: your name
  * @Date: 2020-08-03 21:11:08
- * @LastEditTime: 2020-08-03 23:48:36
+ * @LastEditTime: 2020-08-04 23:48:05
  * @LastEditors: your name
  * @Description:
  * @FilePath: /learn-go-with-tests/structs/premeter.go
  * @可以输入预定的版权声明、个性签名、空行等
  */
 package structs
+
+import (
+	"math"
+)
 
 type Rectangle struct {
 	Width  float64
@@ -16,6 +20,10 @@ type Rectangle struct {
 
 type Circle struct {
 	Radius float64
+}
+
+type Shape interface {
+	Area() float64
 }
 
 func Premeter(width float64, height float64) float64 {
@@ -32,10 +40,12 @@ func Premeter(width float64, height float64) float64 {
 // }
 
 // 申明方法
+// Rectangle 有一个返回值类型为 float64 的方法 Area，所以它满足接口 Shape
 func (r Rectangle) Area() float64 {
-	return 0
+	return r.Width * r.Height
 }
 
+// Circle 有一个返回值类型为 float64 的方法 Area，所以它满足接口 Shape
 func (c Circle) Area() float64 {
-	return 0
+	return math.Pi * c.Radius * c.Radius
 }
