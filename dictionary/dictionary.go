dcae4ac892cdaff6437e462674f28aaef779fec6
“jialin” <914001513@qq.com>
2020-08-12 22:08:57 +0800

    map查找不存在的key

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-08-10 23:25:57
- * @LastEditTime: 2020-08-10 23:46:36
+ * @LastEditTime: 2020-08-12 22:08:05
  * @LastEditors: your name
  * @Description:
  * @FilePath: /learn-go-with-tests/dictionary/dictionary.go
@@ -9,12 +9,25 @@
  */
 package dictionary
 
+import (
+	"errors"
+)
+
 type Dictionary map[string]string
 
 func Search(dictionary map[string]string, word string) string {
 	return dictionary[word]
 }
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+// func (d Dictionary) Search(word string) string {
+// 	return d[word]
+// }
+
+func (d Dictionary) Search(word string) (string, error) {
+	// map 查找的特性，返回两个值。第二个值是一个布尔值，表示是否成功找到 key
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+	return definition, nil
 }
--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -1,6 +1,8 @@
 package dictionary
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSearch(t *testing.T) {
 	dictionary := map[string]string{"test": "this is a test"}
@@ -20,8 +22,25 @@ func assertString(t *testing.T, got string, want string) {
 
 func TestSearch2(t *testing.T) {
 	dictionary := Dictionary{"test": "this is a test"}
-	got := dictionary.Search("test")
-	want := "this is a test"
 
-	assertString(t, got, want)
+	t.Run("know word", func(t *testing.T) {
+		// 返回第二个参数，它是一个 Error 类型
+		got, _ := dictionary.Search("test")
+		want := "this is a test"
+
+		assertString(t, got, want)
+	})
+
+	t.Run("nnknow word", func(t *testing.T) {
+		// 返回第二个参数，它是一个 Error 类型
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertString(t, err.Error(), want)
+	})
+
 }
